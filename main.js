24bdda621fc4ba482b29944f44211735b98e3a8e
Abdul Matin (Dusake) <100327528+dusake-sg@users.noreply.github.com>
2022-09-27 23:59:37 +0800

    change layout

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-const container = document.querySelector('.container');
-let scrollLine = document.querySelector('.scroll');
+let images = [...document.querySelectorAll('.img')];
+let slider = document.querySelector('.slider');
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = 0.05;
 
-container.addEventListener('wheel', (e)=> {
-    e.preventDefault();
-    container.scrollLeft += e.deltaY;
-    scrollLine.style.width = container.scrollLeft / 2 + 'px';
+images.forEach(img, idx => {
+    images.style.backgroundImage = `urls(./assets/${idx+1}.jpeg`
 })
+
+function lerp(start, end, t) {
+    return start * (1-t) + end * t;
+}
+
+function setTransform(el, transform) {
+    el.style.transform = transform;
+}
+
+function init() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth / images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+}
+
+function animate() {
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTransform(slider, `translateX(-${current}px)`)
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
